190dafea3ada15b5e99bb79ac819c0b381e8db95
admin <admin@192.168.10.16>
2016-11-15 09:16:50 +0900

    rtdqr

--- src/com/web/admin/model/LoginService.java
+++ src/com/web/admin/model/LoginService.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component
+@Component("ls")
 public class LoginService {
 
 	@Autowired
